fa560c2c202eb301ff7840fea130bd032b4aaaf7
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-16 16:52:29 +0300

    first commit landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "quiz", to: "pages#quiz", as: :quiz
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
