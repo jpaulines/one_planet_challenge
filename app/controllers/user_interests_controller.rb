f003e8631613dbd6aede91a13b8818cf5f42737d
Emilie Cazaux <emilie.cazaux.dussous@gmail.com>
2019-09-17 16:19:40 +0300

    getting last master

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -1,6 +1,26 @@
 class UserInterestsController < ApplicationController
   def new
-    @user = User.find(params[:user_id])
-    @user_interest = User_interest.new
+    @user = current_user
+    @user_interest = UserInterest.new
+    @user_interest.user = @user
+    authorize @user_interest
+  end
+
+  def create
+    @user = current_user
+    @user_interest = UserInterest.new(user_interest_params)
+    @user_interest.user = @user
+    authorize @user_interest
+    if @user_interest.save!
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_interest_params
+    params.require(:user_interest).permit(:user_id, :category_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  before_action :set_user, only: [:edit]
+  before_action :set_user, only: [:edit, :update]
 
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_interests_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -10,5 +18,9 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
     authorize @user
+  end
+
+  def event_params
+   params.require(:user).permit(:first_name, :last_name, :email, :address)
   end
 end
