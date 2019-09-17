0c52d0336be52e17c81b4bd4c5ccc734d00fd69a
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-17 18:19:01 +0300

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,20 +15,30 @@ class PagesController < ApplicationController
     serialized = File.read(filepath)
     countries = JSON.parse(serialized)
     earth_counter = nil
+
+
+
+    # country = country_name
     countries.each do |country|
       if country["isoa2"] == params[:quiz][:country_code]
         earth_counter = country["value"].to_f.round(1)
-
+        earth_counter = ((earth_counter * 2).ceil.to_f / 2)
       end
-
     end
 
-    redirect_to result_path(result: earth_counter)
+    redirect_to result_path(result: earth_counter, country: params[:quiz][:country_code])
     # {"utf8"=>"✓", "authenticity_token"=>"wp8OQfE+2jQQLD0TgiV/RuERotL6QLDhhY5c/FQfAPRB0JXLp3RbBC/1os/IMadqNpGslFxYhmb2q6HJbffDTw==", "quiz"=>{"country_code"=>"AF"}, "commit"=>"Save Quiz"}
 
   end
 
   def result
     @result = params[:result]
+    @result = ((@result * 2).ceil.to_f / 2)
+    @result_complete = @result.to_i
+    @result_half = @result - @result_complete
+    country_code = params[:country]
+    country = ISO3166::Country[country_code]
+    @country = country.translations[I18n.locale.to_s] || country.name
   end
+
 end
