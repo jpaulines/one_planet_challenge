323ad4397bcbb5eb4d9713cdb8bd9ecfda6a8742
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-17 16:41:36 +0300

    update to quiz

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,34 @@
+require 'json'
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :quiz]
+  skip_before_action :authenticate_user!, only: [ :home, :quiz, :compute_result, :result ]
 
   def home
   end
 
   def quiz
+
   end
 
+  def compute_result
+    filepath = "#{Rails.root}/storage/countries.json"
+    serialized = File.read(filepath)
+    countries = JSON.parse(serialized)
+    earth_counter = nil
+    countries.each do |country|
+      if country["isoa2"] == params[:quiz][:country_code]
+        earth_counter = country["value"].to_f.round(1)
+
+      end
+
+    end
+
+    redirect_to result_path(result: earth_counter)
+    # {"utf8"=>"✓", "authenticity_token"=>"wp8OQfE+2jQQLD0TgiV/RuERotL6QLDhhY5c/FQfAPRB0JXLp3RbBC/1os/IMadqNpGslFxYhmb2q6HJbffDTw==", "quiz"=>{"country_code"=>"AF"}, "commit"=>"Save Quiz"}
+
+  end
+
+  def result
+    @result = params[:result]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   get "quiz", to: "pages#quiz", as: :quiz
+  get "result", to: "pages#result", as: :result
+  post "compute_result", to: "pages#compute_result"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit] do
     resources :user_interest, only: [:new]
