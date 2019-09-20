a6929388efa41f84d833ce8c48d0f1b84af18901
Diana Schoeller <diana.schoeller@student.unisg.ch>
2019-09-20 10:49:26 +0300

    added default to user challenges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_094638) do
+ActiveRecord::Schema.define(version: 2019_09_20_074504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_094638) do
   end
 
   create_table "user_challenges", force: :cascade do |t|
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.bigint "user_id"
     t.bigint "challenge_id"
     t.datetime "created_at", null: false
