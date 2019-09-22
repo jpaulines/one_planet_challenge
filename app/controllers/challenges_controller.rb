7d687e7a0dc13f46fe33e8bcc34560d289842941
Emilie Cazaux <emilie.cazaux.dussous@gmail.com>
2019-09-22 19:01:42 +0300

    to continue

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,7 +1,11 @@
 class ChallengesController < ApplicationController
   before_action :set_challenge, only: [:show]
   def index
-    @challenges = policy_scope(Challenge)
+    if params.present?
+      @challenges = policy_scope(Challenge.where(effort: params[:effort].to_i, money: params[:money].to_i, time: params[:time].to_i))
+        else
+      @challenges = policy_scope(Challenge)
+    end
   end
 
   def show
