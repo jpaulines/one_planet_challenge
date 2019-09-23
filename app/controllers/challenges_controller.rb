1bf3f90cd12c0bb7fd3111f3cd203b858ca28173
Emilie Cazaux <emilie.cazaux.dussous@gmail.com>
2019-09-23 17:54:10 +0300

    index challenges finished

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -8,9 +8,9 @@ class ChallengesController < ApplicationController
       #@challenges = policy_scope(Challenge.where(effort: params[:effort].to_i, money: params[:money].to_i, time: params[:time].to_i))
         else
       @challenge_categories = ""
-      @challenges = policy_scope(Challenge).sort_by {|c| c.created_at}
+      @challenges = policy_scope(Challenge).order(created_at: :desc)
     end
-
+    @popular_challenges = Challenge.all.sort{ |challenge_a, challenge_b| challenge_a.users.count <=> challenge_b.users.count}
   end
 
   def show
